83a8b01c08746e93492fc230c1d68e7899d78632
nhthien-19it1 <nhthien.19it1@vku.udn.vn>
2020-11-07 09:26:58 +0700

    Define Model Schema.

---
+++ src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+let Schema  = mongoose.Schema;
+
+let UserSchema = new Schema({
+  username: String,
+  gender: {type:String,default:"male"},
+  phone:{type:Number,default:null},
+  address:{type:String,default:null},
+  avatar:{type:String,default:"avatar-default.jpg"},
+  role:{type:String,default:"user"},
+  local:{
+    email:{type:String,trim:true},
+    password:String,
+    isActive:{type:Boolean,default:false},
+    verifyToken:String
+  },
+  facebook:{
+    uid:String,
+    token:String,
+    email:{type:String,trim:true}
+  },
+  google:{
+    uid:String,
+    token:String,
+    email:{type:String,trim:true}
+  },
+  createAt:{type:Number,default:Date.now},
+  updateAt:{type:Number,default:null},
+  deleteAt:{type:Number,default:null}
+});
+module.exports = mongoose.model("user",UserSchema);
