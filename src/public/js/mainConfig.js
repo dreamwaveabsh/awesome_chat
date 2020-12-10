1cec92b76f440a4546aedfd2a54f3b4342552975
nhthien-19it1 <nhthien.19it1@vku.udn.vn>
2020-12-10 15:27:14 +0700

    filter_message

--- src/public/js/mainConfig.js
+++ src/public/js/mainConfig.js
@@ -13,15 +13,15 @@ function nineScrollLeft() {
   });
 }
 
-function nineScrollRight() {
-  $('.right .chat').niceScroll({
+function nineScrollRight(divId) {
+  $(`.right .chat[data-chat = ${divId}]`).niceScroll({
     smoothscroll: true,
     horizrailenabled: false,
     cursorcolor: '#ECECEC',
     cursorwidth: '7px',
     scrollspeed: 50
   });
-  $('.right .chat').scrollTop($('.right .chat')[0].scrollHeight);
+  $(`.right .chat[data-chat=${divId}]`).scrollTop( $(`.right .chat[data-chat=${divId}]`)[0].scrollHeight);
 }
 
 function enableEmojioneArea(chatId) {
@@ -84,18 +84,21 @@ function configNotification() {
 }
 
 function gridPhotos(layoutNumber) {
-  let countRows = Math.ceil($('#imagesModal').find('div.all-images>img').length / layoutNumber);
+  $(".show-images").unbind("click").on("click",function(){
+    let href = $(this).attr("href");
+    let modalImagesId = href.replace("#","")
+    let countRows = Math.ceil($(`#${modalImagesId}`).find('div.all-images>img').length / layoutNumber);
   let layoutStr = new Array(countRows).fill(layoutNumber).join("");
-  $('#imagesModal').find('div.all-images').photosetGrid({
+  $(`#${modalImagesId}`).find('div.all-images').photosetGrid({
     highresLinks: true,
     rel: 'withhearts-gallery',
     gutter: '2px',
     layout: layoutStr,
     onComplete: function() {
-      $('.all-images').css({
+      $(`#${modalImagesId}`).find('.all-images').css({
         'visibility': 'visible'
       });
-      $('.all-images a').colorbox({
+      $(`#${modalImagesId}`).find('.all-images a').colorbox({
         photo: true,
         scalePhotos: true,
         maxHeight: '90%',
@@ -103,6 +106,8 @@ function gridPhotos(layoutNumber) {
       });
     }
   });
+  })
+  
 }
 
 
@@ -158,6 +163,10 @@ function changeScreenChat(){
     $(".person").removeClass("active")
     $(this).find("li").addClass("active")
     $(this).tab("show");
+
+    //cấu hình thanh cuộn box chat rightside
+    let divId = $(this).find("li").data("chat")
+    nineScrollRight(divId);
   })
 }
 $(document).ready(function() {
@@ -169,7 +178,7 @@ $(document).ready(function() {
 
   // Cấu hình thanh cuộn
   nineScrollLeft();
-  nineScrollRight();
+
 
   // Bật emoji, tham số truyền vào là id của box nhập nội dung tin nhắn
   enableEmojioneArea("17071995");
